60d66dcc9a29cd3b866be0a23a85f33542b3c70b
Masaaki Matsunaga <castle2164m@gmail.com>
2017-12-10 20:14:41 +0900

    visualized degree of relatedness number

--- analysis.py
+++ analysis.py
@@ -4,9 +4,11 @@
 import re
 import pandas as pd
 from gensim import models, corpora
+from tqdm import tqdm
 import sys
 import os
 import glob
+
 
 SCRIPT_DATA_DIR = './917/'           # Original data which was already handled a monopheme analysis processing.
 CHAR_DATA_DIR = './test_file/'       # Single Words data which was extract from original data.
@@ -44,12 +46,12 @@ def make_train_file(d):
             line = row.strip()
             result_index.append(line)
 
-    return result_text, result_index, file_path
+    return result_text, result_index, file_path, result_index
 
 
 def main():
 
-        r_t, r_i, file_path = make_train_file(CHAR_DATA_DIR)
+        r_t, r_i, file_path, result_index = make_train_file(CHAR_DATA_DIR)
         sentences = LabeledListSentence(r_t, r_i)
         model = models.Doc2Vec(alpha=0.025, min_count=5,
                                size=100, iter=20, workers=4)
@@ -61,13 +63,35 @@ def main():
 
         # detect similar word with its degree of relatedness number.
         print("\n")
+
         x = '神経'
-        print("Analyze about %s" % x)
-        print(model.most_similar(positive=[x]))
+        print("<<Word analyzing about: %s>>" % x)
         print("\n")
+        results = model.most_similar(positive=[x])
+        for result in results:
+            print(result[0])
+
+            with tqdm(total=100) as pbar:
+                for i in range(int(result[1]*1000)):
+                    pbar.update(0.1)
+
+        print("\n")
+
+        # detect similar document with its degree of relatedness number.
         y = 9
-        print("Analyze about %d" % y)
-        print(model.docvecs.most_similar(positive=[y]))
+        print("<<Book's title analyzing about: Index[%d]>>" % y)
+        print("<<Index = %s>>" % result_index)
+
+        print("\n")
+        results1 = model.docvecs.most_similar(positive=[y])
+        for result1 in results1:
+            print(result1[0])
+
+            with tqdm(total=100) as pbar:
+                for i in range(int(result1[1]*1000)):
+                    pbar.update(0.1)
+
+        print("\n")
 
 
 if __name__ == '__main__':
